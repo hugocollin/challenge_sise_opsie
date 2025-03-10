39c5b492146c0d56e3a1a5bf18d60682d60347b4
Falonne Kpamegan <fkpamegan@univ-lyon2.fr>
2025-03-10 11:25:10 +0100

    add .env

--- db/connection.py
+++ db/connection.py
@@ -1,12 +1,21 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
 
-# Configuration de la connexion
+
+
+# Charger les variables d'environnement du fichier .env
+load_dotenv()
+
+# Configuration de la connexion à MariaDB
 config = {
-    "host": "localhost",  
-    "user": "root",
-    "password": "mypass123",
-    "database": "mysql",  
+    "host": os.getenv("DB_HOST"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_NAME"),
+    "port": int(os.getenv("DB_PORT")),
 }
+
 
 try:
     # Connexion à MariaDB
