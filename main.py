a91d13a3204b376d77907d2d07b913972b59053b
Hugo Collin <hugo.collin3@gmail.com>
2025-03-11 08:11:11 +0100

    Correctif

--- main.py
+++ main.py
@@ -2,12 +2,18 @@
 Fichier principal de l'application.
 """
 
+import os
 import streamlit as st
 
+from dotenv import load_dotenv, find_dotenv
 from src.db.connection import load_parquet
 
 # Configuration de la page
 st.set_page_config(page_title="Challenge SISE x OPSIE", page_icon=":material/policy:", layout="wide")
+
+# Récupération de l'environnement d'exécution
+load_dotenv(find_dotenv())
+PLATFORM = os.getenv("PLATFORM")
 
 # Récupération de la page à afficher
 page = st.query_params.page if "page" in st.query_params else "home"
@@ -29,7 +35,10 @@ page_module = __import__(module_name, fromlist=["show"])
 
 # Chargement des données
 if "data" not in st.session_state:
-    data = load_parquet()
+    if PLATFORM == "local":
+        data = load_parquet().persist()
+    else:
+        data = load_parquet()
     st.session_state["data"] = data
 
 # Affichage de la page
